350c4e7bf05f17ce4f2c876f8d20f4395fc43e0f
Errol Silver <esilver@bostoninteractive.com>
2014-06-26 09:27:47 -0400

    no message

--- js/theme.js
+++ js/theme.js
@@ -1,36 +1,37 @@
 function boxExpand() { 
   var $rowItem = $('.wrapper').innerWidth() / $('.boxHead').outerWidth(true);
   var $boxColumns = $('.boxHead').length / $rowItem;
+  var $boxes = $('div.boxContain');
 
   //hide all boxcontents
   $('.boxContent').addClass('hidden');
 
-  //apply target class to last row item
-  $('.boxContain:nth-child(' + $rowItem +'n)').addClass('lastBox');
+  //wrap each "row" of divs in a wrapper to apply margin to on click
+    for (var i = 0; i < $boxes.length; i+=$rowItem) {
+      $boxes.slice(i, i+$rowItem).wrapAll('<div class="boxWrapper"></div>');
+    };
+
 
   
   $('.boxHead').click(function() {
     var $activeContain = $(this).parent($('.boxContain'));
+    var $activeWrap = $activeContain.parent('.boxWrapper');
     var $activeBox = $activeContain.children('.boxContent');
     var $activeHead = $activeContain.children('.boxHead');
+    var $containDistance;
     
-    //shows clicked box, hides others
+    //shows clicked box content, hides others
     $activeBox.toggleClass('hidden');
     $('.boxContent').not($activeBox).addClass('hidden');
     
     //adds space for content to appear, removes if inactive
+    $activeWrap.css("margin-bottom", $activeBox.outerHeight());
+    $('.boxWrapper').not($activeWrap).css('margin-bottom', 0);
 
-    //$activeHead.css("margin-bottom", $activeBox.outerHeight());
-    //('.boxHead').not($activeHead).css('margin-bottom', 0);
-
-
-    $('.boxContain').next('.lastBox').children('.boxHead').css("margin-bottom", $activeBox.outerHeight());
 
     //places content depending on which "row" of headers it falls into
-    var $containDistance = $activeHead.position();
+    $containDistance = $activeHead.position();
     $activeBox.css('top', $containDistance.top + $activeHead.outerHeight());
-
-
   });
 }
 
