ce39f494ffdcb67bbf403386df1bb7feee1435cc
Errol Silver <esilver@bostoninteractive.com>
2014-06-27 12:42:32 -0400

    in progress

--- js/theme.js
+++ js/theme.js
@@ -1,40 +1,51 @@
-function boxExpand() {
-  "use strict";
-  var $rowItem = $('.wrapper').innerWidth() / $('.boxHead').outerWidth(true),
-    $boxes = $('div.boxContain'),
-    $activeContain,
-    $activeWrap,
-    $activeBox,
-    $activeHead,
-    $containDistance,
-    i;
-
-  //hide all boxcontents
-  $('.boxContent').addClass('hidden');
-
-  //wrap each "row" of divs in a wrapper to apply margin to on click
-  for (i = 0; i < $boxes.length; i += $rowItem) {
-    $boxes.slice(i, i + $rowItem).wrapAll('<div class="boxWrapper"></div>');
+(function ($) {
+  function boxWrap() {
+    $(window).resize(function() {
+      setTimeout(function () {
+        $rowItem = Math.ceil($('.wrapper').innerWidth() / $('.boxHead').outerWidth(true));
+        console.log($rowItem);
+      }, 200);
+    });
   }
 
-  $('.boxHead').click(function () {
-    $activeContain = $(this).parent('.boxContain');
-    $activeWrap = $activeContain.parent('.boxWrapper');
-    $activeBox = $activeContain.children('.boxContent');
-    $activeHead = $activeContain.children('.boxHead');
+  function boxExpand() {
+    var $rowItem = Math.floor($('.wrapper').innerWidth() / $('.boxHead').outerWidth(true)),
+      $boxes = $('div.boxContain'),
+      $activeContain,
+      $activeWrap,
+      $activeBox,
+      $activeHead,
+      $containDistance,
+      i;
 
-  //shows clicked box content, hides others
-    $activeBox.toggleClass('hidden');
-    $('.boxContent').not($activeBox).addClass('hidden');
+    //hide all boxcontents
+    $('.boxContent').addClass('hidden');
 
-  //adds space for content to appear, removes if inactive
-    $activeWrap.css("margin-bottom", $activeBox.outerHeight());
-    $('.boxWrapper').not($activeWrap).css('margin-bottom', 0);
+    //wrap each "row" of divs in a wrapper to apply margin to on click
+    for (i = 0; i < $boxes.length; i += $rowItem) {
+      $boxes.slice(i, i + $rowItem).wrapAll('<div class="boxWrapper"></div>');
+    }
+
+    $('.boxHead').click(function () {
+      $activeContain = $(this).parent('.boxContain');
+      $activeWrap = $activeContain.parent('.boxWrapper');
+      $activeBox = $activeContain.children('.boxContent');
+      $activeHead = $activeContain.children('.boxHead');
+
+    //shows clicked box content, hides others
+      $activeBox.toggleClass('hidden');
+      $('.boxContent').not($activeBox).addClass('hidden');
+
+    //adds space for content to appear, removes if inactive
+      $activeWrap.css("margin-bottom", $activeBox.outerHeight());
+      $('.boxWrapper').not($activeWrap).css('margin-bottom', 0);
 
 
-    //places content depending on which "row" of headers it falls into
-    $containDistance = $activeHead.position();
-    $activeBox.css('top', $containDistance.top + $activeHead.outerHeight());
-  });
-}
-boxExpand();
+      //places content depending on which "row" of headers it falls into
+      $containDistance = $activeHead.position();
+      $activeBox.css('top', $containDistance.top + $activeHead.outerHeight());
+    });
+  }
+  boxWrap();
+  boxExpand();
+}(jQuery));
