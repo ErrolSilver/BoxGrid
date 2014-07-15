5bb5ff9b7a615fcc3c2200e22c0232eeaa28deb5
Errol Silver <esilver@bostoninteractive.com>
2014-07-15 17:15:26 -0400

    no message

--- js/theme.js
+++ js/theme.js
@@ -6,9 +6,17 @@
       $activeBox,
       $activeHead,
       $containDistance,
+      $savedHeights = [],
       i;
 
+      // Stores content height for use as margin later
+      $('.boxContent').each(function() {
+        $savedHeights.push($(this).outerHeight(true));
+      });
+
   function boxWrap() {
+
+    // Re-Usable function that fires only once the window is done resizing
     var onResize = (function () {
       var timers = {};
       return function (callback, ms, uniqueId) {
@@ -22,64 +30,90 @@
       }
     })();
 
+
     $(window).resize(function() {
+      // sets rowItem to according to window size
       var $rowItem = Math.ceil($('.wrapper').innerWidth() / $('.boxHead').outerWidth(true));
-      console.log($rowItem);
+
       onResize(function() {
+
+      // Removes existing wrapper
        $boxes.unwrap();
+
+       // Applies new wrapper
         setTimeout(function () {
           for (i = 0; i < $boxes.length; i += $rowItem) {
             $boxes.slice(i, i + $rowItem).wrapAll('<div class="boxWrapper"></div>')
           }
-        },1);
-        
+        },1); // Staggers the wrapping and unwrapping  
       }, 2, 'box wrap');
-      $('.boxContent').addClass('notShown');
+
+      // Hides any open content on resize
+      $('.boxContent').css({
+        height: 0,
+        padding: 0
+      })
     });
-
-
-
   }
 
   function boxExpand() {
+
+    //Initial wrapping
     var $rowItem = Math.floor($('.wrapper').innerWidth() / $('.boxHead').outerWidth(true));
 
     for (i = 0; i < $boxes.length; i += $rowItem) {
       $boxes.slice(i, i + $rowItem).wrapAll('<div class="boxWrapper"></div>')
     }
 
-    // hide all boxcontents
-    $('.boxContent').addClass('notShown');
-
+    $('.boxWrapper').css('margin-bottom', '0');
+    // Hide all box contents
+    $('.boxContent').css({
+      height: '0',
+      padding: '0'
+    });
 
     $('.boxHead').click(function () {
       $activeContain = $(this).parent('.boxContain');
       $activeWrap = $activeContain.parent('.boxWrapper');
       $activeBox = $activeContain.children('.boxContent');
       $activeHead = $activeContain.children('.boxHead');
+      $boxIndex = $('.boxHead').index($(this));
 
-    // shows clicked box content, hides others
-      $activeBox.toggleClass('notShown');
-      $('.boxContent').not($activeBox).addClass('notShown');
-
-    // adds space for content to appear, removes if inactive
-
-      $activeWrap.velocity({
-        "margin-bottom":  $activeBox.outerHeight()
-      }, 200);
-      $activeWrap.css("margin-bottom", $activeBox.outerHeight());
-      
-      //$('.boxWrapper').not($activeWrap).css('margin-bottom', 0);
+    // Shows clicked box content, hides others
+    console.log($(this).parent('.boxWrapper'));
+      if ($(this).parent('.boxWrapper').css('margin-bottom') != 0) {
+        console.log("its not zero");
+      } else {
+        console.log("its zero");
+      }   // NEEDS ATTENTION 
 
 
-      // places content depending on which "row" of headers it falls into
-      $containDistance = $activeHead.position();
 
       $activeBox.velocity({
-        top: $containDistance.top + $activeHead.outerHeight()
+        height:  $savedHeights[$boxIndex],
+        padding: '2.5%'
+      }, 200); 
+
+      $('.boxContent').not($activeBox).velocity({
+        height: 0,
+        padding: 0
       }, 200);
-      //$activeBox.css('top', $containDistance.top + $activeHead.outerHeight());
+
+    // Adds space for content to appear, removes if inactive
+      $activeWrap.velocity({
+        "margin-bottom":  $savedHeights[$boxIndex]
+      }, 200);
+      
+      $('.boxWrapper').not($activeWrap).velocity({'margin-bottom': 0}, 200);
+
+
+      // Places content depending on which "row" of headers it falls into
+      $containDistance = $activeHead.position();
+
+      // Adds space so the header doesn't overlap the content
+      $activeBox.css('top', $containDistance.top + $activeHead.outerHeight());
     });
+
   }
   boxWrap();
   boxExpand();
